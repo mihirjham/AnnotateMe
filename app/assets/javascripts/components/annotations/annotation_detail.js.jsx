106f77a1c980e714de4d7bb45de1af3a7d1e35fe
Mihir Jham <mihirjham@gmail.com>
2015-10-22 13:34:57 -0700

    Can add images and links to annotations now

--- app/assets/javascripts/components/annotations/annotation_detail.js.jsx
+++ app/assets/javascripts/components/annotations/annotation_detail.js.jsx
@@ -1,6 +1,10 @@
 (function(root) {
   'use strict';
 
+  var strSplice = function(str, index, count, add){
+    return str.slice(0, index) + (add || "") + str.slice(index + count);
+  };
+
   var AnnotationDetail = root.AnnotationDetail = React.createClass({
     mixins: [ReactRouter.History],
     getInitialState: function(){
@@ -32,6 +36,30 @@
     handleUserClick: function(){
       this.history.pushState(null, "/users/" + this.state.annotation.user_id);
     },
+    componentDidUpdate: function(){
+      var content = document.getElementsByClassName("annotation-detail")[0].innerHTML;
+      var index = 0;
+
+      var urls = content.match(/(?:^|[^"'])(\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|])/gim);
+
+      urls.forEach(function(url){
+        var urlIndex = content.indexOf(url, index);
+
+        if(url.match(/\.(png|jpeg|jpg|gif)$/) === null){
+          if(content.indexOf("<a href", index) === -1){
+            content = strSplice(content, urlIndex, url.length, "<a href='" + url + "'>" + url + "</a>");
+            index += (url.length * 2) + 15;
+          }
+        }else{
+          if(content.indexOf("<img src", index) === -1){
+            content = strSplice(content, urlIndex, url.length, "<img src ='" + url + "' />");
+            index += url.length + 14;
+          }
+        }
+      });
+
+      document.getElementsByClassName("annotation-detail")[0].innerHTML = content;
+    },
     render: function(){
       if(this.state.annotation === undefined){
         return(<div></div>);
@@ -43,7 +71,7 @@
             <span className="contributor-label"><a className=""onClick={this.handleUserClick}>{this.state.annotation.email}</a></span>
           </div>
           <div className="annotation">
-            <p>
+            <p className="annotation-detail">
               {
                 this.state.annotation.annotation
               }
