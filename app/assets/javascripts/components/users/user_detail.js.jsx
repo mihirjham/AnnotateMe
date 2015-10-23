48395e3bd7ce4165e370e1b05562eb75066d5069
Mihir Jham <mihirjham@gmail.com>
2015-10-23 10:15:45 -0700

    Users comments can be seen on their profile page.

--- app/assets/javascripts/components/users/user_detail.js.jsx
+++ app/assets/javascripts/components/users/user_detail.js.jsx
@@ -1,10 +1,14 @@
 (function(root) {
   'use strict';
 
+  var strSplice = function(str, index, count, add){
+    return str.slice(0, index) + (add || "") + str.slice(index + count);
+  };
+
   var UserDetail = root.UserDetail = React.createClass({
     mixins: [ReactRouter.History],
     getInitialState: function(){
-      return {user: UserStore.currentUser()};
+      return {user: UserStore.currentUser(), selectedPane: "annotations"};
     },
     componentDidMount: function(){
       UserStore.addChangeListener(this._onChange);
@@ -27,6 +31,76 @@
     handleSongClick: function(annotation){
       this.history.pushState(null, "/songs/" + annotation.song_id.toString());
     },
+    handleAnnotationsClick: function(){
+      this.setState({selectedPane: "annotations"});
+    },
+    handleCommentsClick: function(){
+      this.setState({selectedPane: "comments"});
+    },
+    formatAnnotations: function(){
+      var annotations = document.getElementsByClassName("annotation-feed-annotation");
+
+      for(var i = 0; i < annotations.length; i++){
+        var content = annotations[i].innerHTML;
+        var index = 0;
+
+        var urls = content.match(/(?:^|[^"'])(\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|])/gim);
+        if(urls === null){
+          continue;
+        }
+
+        urls.forEach(function(url){
+          var urlIndex = content.indexOf(url, index);
+
+          if(url.match(/\.(png|jpeg|jpg|gif)$/) === null){
+            if(content.indexOf("<a href", index) === -1){
+              content = strSplice(content, urlIndex, url.length, "<a href='" + url + "'>" + url + "</a>");
+              index += (url.length * 2) + 15;
+            }
+          }else{
+            if(content.indexOf("<img src", index) === -1){
+              content = strSplice(content, urlIndex, url.length, "<img src ='" + url + "' />");
+              index += url.length + 14;
+            }
+          }
+        });
+        annotations[i].innerHTML = content;
+      }
+    },
+    formatComments: function(){
+      var comments = document.getElementsByClassName("comment");
+
+      for(var i = 0; i < comments.length; i++){
+        var content = comments[i].innerHTML;
+        var index = 0;
+
+        var urls = content.match(/(?:^|[^"'])(\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|])/gim);
+        if(urls === null){
+          continue;
+        }
+
+        urls.forEach(function(url){
+          var urlIndex = content.indexOf(url, index);
+
+          if(url.match(/\.(png|jpeg|jpg|gif)$/) === null){
+            if(content.indexOf("<a href", index) === -1){
+              content = strSplice(content, urlIndex, url.length, "<a href='" + url + "'>" + url + "</a>");
+              index += (url.length * 2) + 15;
+            }
+          }else{
+            if(content.indexOf("<img src", index) === -1){
+              content = strSplice(content, urlIndex, url.length, "<img src ='" + url + "' />");
+              index += url.length + 14;
+            }
+          }
+        });
+        comments[i].innerHTML = content;
+      }
+    },
+    componentDidUpdate: function(){
+      this.formatAnnotations();
+      this.formatComments();
+    },
     render: function(){
       if(this.state.user === undefined){
         return(<div></div>);
@@ -41,6 +115,47 @@
       }
       var img = <img className="artist_image" src={this.state.user.cloudinary_url}/>;
 
+      var annotations = <div className="annotations-feed tab-pane">
+                      <div className="annotations">
+                        {
+                          this.state.user.annotations.map(function(annotation){
+                            return(
+                              <div key={annotation.id} className="annotation-feed-item">
+                                <blockquote>
+                                  <p className="annotation-snippet">{annotation.snippet}</p>
+                                  <footer>
+                                    <a onClick={this.handleSongClick.bind(null, annotation)}>{annotation.song_name}</a> by {this.state.user.email}
+                                  </footer>
+                                </blockquote>
+                                <p className="annotation-feed-annotation">
+                                  {annotation.annotation}
+                                </p>
+                              </div>
+                            );
+                          }.bind(this))
+                        }
+                      </div>
+                    </div>;
+
+        var comments =  <div className="comments-feed">
+                        <div className="comments">
+                          {
+                            this.state.user.comments.map(function(comment){
+                              return(
+                                <div key={comment.id} className="comments-feed-item">
+                                  <blockquote>
+                                    <p className="comment">{comment.comment}</p>
+                                    <footer>
+                                      {comment.name}
+                                    </footer>
+                                  </blockquote>
+                                </div>
+                              );
+                            })
+                          }
+                        </div>
+                      </div>;
+
       return(
         <div className="user_page">
           <div className="user_information">
@@ -51,34 +166,16 @@
             </div>
             <div className="user_email"><h1>{this.state.user.email}</h1></div>
           </div>
-          <div className="user_activity">
+          <div className="user_activity" >
             <ul className="nav nav-tabs">
-              <li><a>Annotations
+              <li><a onClick={this.handleAnnotationsClick}>Annotations
                 <span className="badge">{this.state.user.annotations.length}</span>
               </a></li>
+              <li><a onClick={this.handleCommentsClick}>Comments
+                <span className="badge">{this.state.user.comments.length}</span>
+              </a></li>
             </ul>
-            <div className="annotations-feed">
-              <div className="annotations">
-                {
-                  this.state.user.annotations.map(function(annotation){
-                    return(
-                      <div key={annotation.id} className="annotation-feed-item">
-                        <blockquote>
-                          <p>{annotation.snippet}</p>
-                          <footer>
-                            <a onClick={this.handleSongClick.bind(null, annotation)}>{annotation.song_name}</a> by {this.state.user.email}
-                          </footer>
-                        </blockquote>
-                        <p className="annotation-feed-annotation">
-                          {annotation.annotation}
-                        </p>
-                      </div>
-                    );
-                  }.bind(this))
-                }
-
-              </div>
-            </div>
+            {this.state.selectedPane === "annotations" ? annotations : comments}
           </div>
         </div>
       );
--- app/views/api/comments/_comment.json.jbuilder
+++ app/views/api/comments/_comment.json.jbuilder
@@ -1,2 +1,3 @@
 json.extract! comment, :id, :comment, :commentable_id, :commentable_type, :user_id, :created_at, :updated_at
 json.email comment.user.email
+json.name comment.commentable.name
